259d66c20d2558124acb407975322821f6250fe6
Jair Medina <54874269+JairMedina97@users.noreply.github.com>
2021-08-05 15:32:12 -0500

    Add files via upload

---
+++ intro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// intefer = numero entero
+int main() { // main routine (where execution starts)
+    int a, b, c, d, e; //variable
+    a = 123; // asignment
+    b = 15;
+    c = a % b;
+    d = a / b;
+    e = d * b +c;
+    printf("%i is the same as %i", e, a); // imprimir
+    return 0;
+}
